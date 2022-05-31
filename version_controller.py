651a89f5dab4ed366f71b71a2de098830d0dea55
Ami Hazbany <ami@orbs.com>
2022-05-31 15:03:46 +0300

    version control changes

--- version_controller.py
+++ version_controller.py
@@ -1,26 +1,43 @@
 import os
-from pathlib import Path
-import sys
 import time
-from datetime import datetime
-import logging
 import json
 from urllib import request
-
-sys.path.append('/usr/src/mytonctrl')
-
-import mytonctrl
-from mypylib.mypylib import *
+from packaging import version
+from subprocess import call
 
 
 class VersionController(object):
 	VERSION_DESCRIPTOR = 'https://raw.githubusercontent.com/defi-org-code/ton-validator-reporter/master/version.txt'
+	INSTALLER_DESCRIPTOR = 'https://raw.githubusercontent.com/defi-org-code/ton-validator-reporter/master/installer.sh'
+
+	REPORTER_DIR = f'/var/ton-validator-reporter'
+	REPORTER_PARAMS_FILE = f'{REPORTER_DIR}/params.json'
 
 	SECONDS_IN_YEAR = 365 * 24 * 3600
-	SLEEP_INTERVAL = 60
+	SLEEP_INTERVAL = 5 * 60
+	version = None
 
 	def __init__(self):
 		super(VersionController, self).__init__()
+		self.version = self.get_version()
+
+	def get_version(self):
+
+		print(f'reading version file from {self.REPORTER_PARAMS_FILE}')
+
+		orbs_validator_params = {}
+		if os.path.isfile(self.REPORTER_PARAMS_FILE):
+			with open(self.REPORTER_PARAMS_FILE, 'r') as f:
+				orbs_validator_params = json.load(f)
+
+		if 'version' not in orbs_validator_params:
+			orbs_validator_params['version'] = '0.0.0'
+			print(f'updating {self.REPORTER_PARAMS_FILE} with version {orbs_validator_params["version"]}')
+			with open(self.REPORTER_PARAMS_FILE, 'w') as f:
+				json.dump(orbs_validator_params, f)
+
+		print(f'current version is {orbs_validator_params["version"]}')
+		return version.Version(orbs_validator_params['version'])
 
 	def run(self):
 
@@ -28,14 +45,24 @@ class VersionController(object):
 
 			try:
 				with request.urlopen(self.VERSION_DESCRIPTOR) as url:
-					data = json.loads(url.read().decode())
+					data = url.read().decode().strip()
+					curr_version = version.parse(data)
 
-					print(data)
+					print(curr_version, self.version)
+
+					if curr_version > self.version:
+
+						if os.path.exists('installer.sh'):
+							print('removing old installer.sh')
+							os.remove('installer.sh')
+
+						print('downloading new installer.sh')
+						request.urlretrieve(self.INSTALLER_DESCRIPTOR, 'installer.sh')
+						os.chmod('installer.sh', 777)
+						call("./installer.sh")
 
 			except Exception as e:
 				print(e)
-				# self.log.info('unexpected error: ', e)
-				# self.log.info(res)
 
 			sleep_sec = self.SLEEP_INTERVAL - time.time() % self.SLEEP_INTERVAL
 			# self.log.info(f'sleep for {round(sleep_sec)} seconds')
