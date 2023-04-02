a470d62df232bbec2c93a8e1bdee13b916b0c6dd
doron aviguy <doronaviguy@gmail.com>
2023-04-02 15:23:38 +0300

    Update upgrader.sh

--- upgrader.sh
+++ upgrader.sh
@@ -8,6 +8,6 @@ echo 'uninstall reporter completed'
 rm reporter-install.sh
 wget https://raw.githubusercontent.com/defi-org-code/ton-validator-reporter/master/reporter-install.sh
 chmod +x reporter-install.sh
-sudo ./reporter-install.sh 
+./reporter-install.sh 
 
 echo 'install and upgrader reporter completed , run systemctl status reporter'
